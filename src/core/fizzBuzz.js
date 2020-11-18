dcbb77b7710f853ca6f9bc2b93042c337ee80e4d
emmanuel.conrardy <emmanuel.conrardy@acadomia.fr>
2020-11-18 16:51:34 +0100

    clean

--- src/core/fizzBuzz.js
+++ src/core/fizzBuzz.js
@@ -1,20 +1,6 @@
 
 function fizzBuzz(n) { 
-  for (var i = 1; i <= 100; i++ ) {
-    const divisibleBy = (divider, n) => n % divider == 0;
-  
-    if(divisibleBy(15, n)) 
-      return "fizzbuzz";
-  
-    if(divisibleBy(3, n)) 
-      return "fizz";
-  
-    if(divisibleBy(5, n)) 
-      return "buzz";
-  
-    return n; 
-  
-  }
+
 } 
   
 
